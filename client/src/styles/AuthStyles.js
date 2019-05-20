ebad1ccb13b90477689181cee0512435dfc346b2
Chad Jemmett <chad.jemmett@gmail.com>
2019-05-20 17:36:30 -0600

    Created a better structure for the styled components

--- client/src/styles/AuthStyles.js
+++ client/src/styles/AuthStyles.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
+// Signup Page styles
 
 export const Button = styled.button`
+  // write styles in here just like a CSS file
   background: blue;
 
 `
@@ -11,4 +13,8 @@ export const SignupForm = styled.form`
   border: black solid 1px;
 
 `
+
+// Login page styles
+
+
 
