73cf0209e6ae8fb95a97eb4a3a0eec544ec7cbd0
Jim Schlader <jamesschlader@yahoo.com>
2019-05-29 19:48:15 -0600

    Select dropdown with skills works with data binding.

--- client/src/Auth/Login.js
+++ client/src/Auth/Login.js
@@ -1,68 +1,70 @@
-import React, { Component } from 'react';
-import { withContext } from "../AppContext"
-// apppaul 
+import React, { Component } from "react";
+import { withContext } from "../AppContext";
+// apppaul
 class LoginForm extends Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "",
-            password: "",
-            errorMessage: ""
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      errorMessage: ""
+    };
+  }
 
-    handleChange = (e) => {
-        const { name, value } = e.target
-        this.setState({
-            [name]: value
-        })
-    }
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
-    clearInputs = () => {
-        this.setState({
-            username: "",
-            password: "",
-            errorMessage: ""
-        })
-    }
+  clearInputs = () => {
+    this.setState({
+      username: "",
+      password: "",
+      errorMessage: ""
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.login(this.state)
-            .then(() => this.props.history.push("/todos"))
-            .catch(err => {
-                this.setState({errorMessage: err.response.data.message})
-            })
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props
+      .login(this.state)
+      .then(() => this.props.history.push("/todos"))
+      .catch(err => {
+        this.setState({ errorMessage: err.response.data.message });
+      });
+  };
 
-    render() {
-        return (
-            <div className="form-wrapper">
-                <form onSubmit={this.handleSubmit}>
-                    <h3>Log In</h3>
-                    <input
-                        onChange={this.handleChange}
-                        value={this.state.username}
-                        name="username"
-                        type="text"
-                        placeholder="username"/>
-                    <input
-                        onChange={this.handleChange}
-                        value={this.state.password}
-                        name="password"
-                        type="password"
-                        placeholder="password"/>
-                    <button type="submit">Submit</button>
-                </form>
+  render() {
+    return (
+      <div className="form-wrapper">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Log In</h3>
 
-                {
-                    this.state.errorMessage &&
-                    <p style={{color: "red"}}>{this.state.errorMessage}</p>
-                }
+          <input
+            onChange={this.handleChange}
+            value={this.state.username}
+            name="username"
+            type="text"
+            placeholder="username"
+          />
+          <input
+            onChange={this.handleChange}
+            value={this.state.password}
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+          <button type="submit">Submit</button>
+        </form>
 
-            </div>
-        )
-    }
+        {this.state.errorMessage && (
+          <p style={{ color: "red" }}>{this.state.errorMessage}</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default withContext(LoginForm);
--- client/src/Auth/Signup.js
+++ client/src/Auth/Signup.js
@@ -1,77 +1,88 @@
-import React, { Component } from 'react';
-import { withContext } from "../AppContext"
-import styled from 'styled-components'
-import { Button, SignupForm, Inputs } from "../styles/AuthStyles.js"
-import { H1 } from "../styles/GlobalStyles.js"
-
-
+import React, { Component } from "react";
+import { withContext } from "../AppContext";
+import styled from "styled-components";
+import { Button, SignupForm, Inputs, Select } from "../styles/AuthStyles.js";
+import { H1 } from "../styles/GlobalStyles.js";
+import { skills } from "../constants/skills";
 
 class Signup extends Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "",
-            password: "",
-            errorMessage: ""
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      errorMessage: "",
+      skill: null
+    };
+  }
 
-    handleChange = (e) => {
-        const { name, value } = e.target
-        this.setState({
-            [name]: value
-        })
-    }
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
-    clearInputs = () => {
-        this.setState({
-            username: "",
-            password: "",
-            errorMessage: ""
-        })
-    }
+  clearInputs = () => {
+    this.setState({
+      username: "",
+      password: "",
+      errorMessage: ""
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.signup(this.state)
-            .then(() => this.props.history.push("/todos"))
-            .catch(err => {
-                this.setState({errorMessage: err.response.data.message})
-            })
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props
+      .signup(this.state)
+      .then(() => this.props.history.push("/todos"))
+      .catch(err => {
+        this.setState({ errorMessage: err.response.data.message });
+      });
+  };
 
+  render() {
+    return (
+      <div className="form-wrapper">
+        <SignupForm onSubmit={this.handleSubmit}>
+          <H1>Sign Up</H1>
+          <Select
+            type="select"
+            name="skill"
+            header="Skills"
+            onChange={this.handleChange}
+          >
+            <option>Choose a skill</option>
+            {skills.map((item, index) => (
+              <option key={index} value={index}>
+                {item}
+              </option>
+            ))}
+          </Select>
+          {this.state.skill ? skills[this.state.skill] : null}
+          <Inputs
+            onChange={this.handleChange}
+            value={this.state.username}
+            name="username"
+            type="text"
+            placeholder="Username"
+          />
+          <Inputs
+            onChange={this.handleChange}
+            value={this.state.password}
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+          <Button type="submit">Create Account</Button>
+        </SignupForm>
 
-    render() {
-        return (
-            <div className="form-wrapper">
-                <SignupForm onSubmit={this.handleSubmit}>
-                    <H1>Sign Up</H1>
-                    <Inputs
-                        onChange={this.handleChange}
-                        value={this.state.username}
-                        name="username"
-                        type="text"
-                        placeholder="Username"/>
-                    <Inputs
-                        onChange={this.handleChange}
-                        value={this.state.password}
-                        name="password"
-                        type="password"
-                        placeholder="Password"/>
-                    <Button type="submit">Create Account</Button>
-                </SignupForm>
-
-                {
-                    this.state.errorMessage &&
-                    <p style={{color: "red"}}>{this.state.errorMessage}</p>
-                }
-
-            </div>
-        )
-    }
+        {this.state.errorMessage && (
+          <p style={{ color: "red" }}>{this.state.errorMessage}</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default withContext(Signup);
-
-
-
