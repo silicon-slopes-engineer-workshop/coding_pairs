4c9ca6601d5c2814905631ad3519f9749e5fc409
murffious <titan.murff@gmail.com>
2019-06-19 20:25:02 -0600

    front end done mostly

--- client/src/AppContext.js
+++ client/src/AppContext.js
@@ -14,7 +14,8 @@ export class AppContextProvider extends Component {
     constructor() {
         super()
         this.state = {
-            todos: [],
+            users: [],
+            pairs: [],
             user: JSON.parse(localStorage.getItem("user")) || {},
             token: localStorage.getItem("token") || ""
         }
@@ -32,11 +33,29 @@ export class AppContextProvider extends Component {
             })
     }
 
+    getParticipants = () => {
+        return todoAxios.get("/api/admin/participants")
+            .then(response => {
+                this.setState({ users: response.data });
+                return response;
+            })
+    }
+
     addTodo = (newTodo) => {
         return todoAxios.post("/api/todo/", newTodo)
             .then(response => {
                 this.setState(prevState => {
                     return { todos: [...prevState.todos, response.data] }
+                });
+                return response;
+            })
+    }
+
+   makePairs = (newParticipants) => {
+        return todoAxios.post("/api/admin/pairs", newParticipants)
+            .then(response => {
+                this.setState(prevState => {
+                    return { pairs: [...prevState.pairs, response.data] }
                 });
                 return response;
             })
