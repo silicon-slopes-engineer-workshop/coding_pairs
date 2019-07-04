0efa80050559470f9accec732a7a87b3d2aa3483
murffious <titan.murff@gmail.com>
2019-07-04 02:55:30 -0600

    more jazz techno digital man

--- api/index.js
+++ api/index.js
@@ -7,22 +7,8 @@ const expressJwt = require("express-jwt");
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-// Multi-process to utilize all CPU cores.
-if (cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
-
-} else {
   const app = express();
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -62,12 +48,9 @@ if (cluster.isMaster) {
     return res.send({ message: err.message });
   });
 
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
+
 
   app.listen(PORT, function () {
-    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+    console.error(`Node listening on port ${PORT}`);
   });
-}
+
